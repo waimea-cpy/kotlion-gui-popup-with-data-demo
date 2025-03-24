ceaac4986bf70b0ac540457b20fc7ce17b1c776b
Steve Copley <steve.copley@waimea.school.nz>
2025-03-24 13:50:29 +1300

    Update info comment

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,15 @@
  * Kotlin GUI Pop-Up Dialog Demo with Data Model
  * ===============================================================
  *
- * 
+ * This is a demo of how to create and manage a pop-up dialog
+ * window as part of your application.
+ *
+ * The pop-up is modal - this means that it retains focus, and the
+ * main window can't be interacted with until the pop-up is closed.
+ *
+ * In this example, the pop-up is linked to the app data model and
+ * to the main window so that it can access and update the data
+ * model as needed
  */
 
 import com.formdev.flatlaf.FlatDarkLaf
